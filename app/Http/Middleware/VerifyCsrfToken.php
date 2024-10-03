b37d29b69bab0d67b313f6323978af6fe2e78f2f
tony7891234 <f4123838@gmail.com>
2024-10-03 23:41:47 +0800

    mid

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -9,9 +9,13 @@ class VerifyCsrfToken extends Middleware
     /**
      * The URIs that should be excluded from CSRF verification.
      *
-     * @var array<int, string>
+     * @var array
      */
+
     protected $except = [
-        //
+        'notify',//  回调
+        '/telegram/webhook',// telegram
+        'india',
+        'india2',
     ];
 }
