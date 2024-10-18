66a1691205d7d07cba77ae0a923d734bd8ae8ac6
tony7891234 <103761593+tony7891234@users.noreply.github.com>
2024-10-19 00:35:04 +0800

    1

--- app/Console/Commands/NotifyCommand.php
+++ app/Console/Commands/NotifyCommand.php
@@ -105,7 +105,7 @@ class NotifyCommand extends BaseCommand
             ->where('status', '<', 2)
             ->where('notify_num', '=', 0)
             ->orderBy('order_id', 'asc')
-            ->limit(800)
+            ->limit(1300)
             ->get();
 
         $this->sql_finished = time(); // sql 结束时间
