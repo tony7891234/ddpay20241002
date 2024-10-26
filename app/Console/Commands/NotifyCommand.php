8d29abd57aaf4fdaa67d369fd8910613f7989feb
tony7891234 <103761593+tony7891234@users.noreply.github.com>
2024-10-26 09:33:42 +0800

    8

--- app/Console/Commands/NotifyCommand.php
+++ app/Console/Commands/NotifyCommand.php
@@ -67,7 +67,7 @@ class NotifyCommand extends BaseCommand
         $this->start_at = time();
 
         // 10.17号，改成只处理一个小时之内的数据，不然可能需要的时间长
-        $create_time = time() - 3600 * 10;
+        $create_time = time() - 3600 * 5;
         $this->count_order = RechargeOrder::where('create_time', '>', $create_time)
             ->where('notify_status', RechargeOrder::NOTIFY_STATUS_WAITING)
             ->where('status', '<', 2)
