c22c267e19dd7c09e6277d2fc29cf21aac43038a
tony7891234 <103761593+tony7891234@users.noreply.github.com>
2024-11-19 12:37:36 +0800

    1

--- app/Admin/Controllers/RechargeOrderController.php
+++ app/Admin/Controllers/RechargeOrderController.php
@@ -103,10 +103,11 @@ class RechargeOrderController extends AdminController
         $grid->column('orderid', '系统订单号'); // 直接对此字段查询
         $grid->column('amount', '金额');
         $grid->column('notify_num', '回调次数');
-        $grid->column('account', '收款账号')->hide();
+        $grid->column('bank_open', '通道')->hide();
         $grid->column('sf_id', '三方订单号')->hide();
         $grid->column('yh_bq', '银行标签/E2E')->hide();
         $grid->column('kouling', 'ConciliationId')->hide();
+        $grid->column('account', '收款账号')->hide();
         $grid->column('bankname', '开户行');
         $grid->column('status', '订单状态')->display(function ($input) {
             return isset(RechargeOrder::LIST_STATUS[$input]) ? RechargeOrder::LIST_STATUS[$input] : $input;
