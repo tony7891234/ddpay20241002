59963daa6fd473386bebf0890422ef139443d8fc
tony7891234 <103761593+tony7891234@users.noreply.github.com>
2024-11-21 22:03:44 +0800

    1

--- config/queue.php
+++ config/queue.php
@@ -37,7 +37,7 @@ return [
         'database' => [
             'driver' => 'database',
             'table' => 'jobs',
-            'queue' => 'dcat_admin',
+            'queue' => 'default',
             'retry_after' => 90,
             'after_commit' => false,
         ],
