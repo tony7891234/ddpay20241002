647b720b2fb0a296944e182994e5c56019d37639
tony <103761593+tony7891234@users.noreply.github.com>
2025-11-22 15:38:18 +0800

    1

--- app/Admin/Controllers/RechargeOrder3Controller.php
+++ app/Admin/Controllers/RechargeOrder3Controller.php
@@ -24,7 +24,7 @@ class RechargeOrder3Controller extends AdminController
     {
 
         $model = new RechargeOrder();
-        $tableName = 'cd_order_250828';
+        $tableName = 'cd_order_250101';
         $model->setConnection('rds')->setTable($tableName);
 
         /**
@@ -35,28 +35,20 @@ class RechargeOrder3Controller extends AdminController
 
         // 表格导出的字段
         $titles = [
-            'id' => 'UID',
-            'merchantid' => '商户ID',
             'orderid' => '系统订单号',
             'amount' => '订单金额',
             'status' => '订单状态',
-            'inizt' => '订单类型',
             'account' => '收款账号',
             'bankname' => '开户行',
             'create_time' => '下单时间',
-            'completetime' => '完成时间',
         ];
-
         // 表格导出文件名
         $fileName = $this->title();
 
         // 表格导出
         $grid->export()->titles($titles)->rows(function (array $rows) {
             foreach ($rows as $index => &$row) {
-                $row['status'] = isset(RechargeOrder::LIST_STATUS[$row['status']]) ? RechargeOrder::LIST_STATUS[$row['status']] : $row['status'];
-                $row['inizt'] = isset(RechargeOrder::LIST_INIZT[$row['inizt']]) ? RechargeOrder::LIST_INIZT[$row['inizt']] : $row['inizt'];
                 $row['create_time'] = date('Y-m-d H:i:s', $row['create_time']);
-                $row['completetime'] = date('Y-m-d H:i:s', $row['completetime']);
             }
             return $rows;
         })->filename($fileName)->xlsx();
@@ -91,38 +83,28 @@ class RechargeOrder3Controller extends AdminController
 
 
         //  搜索条件
-        $grid->model()->orderBy('create_time', 'desc'); // 按照ID 倒序排序
+        $grid->model()
+            ->select([
+                'id',           // ⚠️ 必须包含主键，否则Grid内部逻辑可能会报错
+                'order_id',     // 列表显示用到
+                'orderid',      // 列表和导出用到
+                'amount',       // 列表和导出用到
+                'status',       // 导出用到
+                'account',      // 列表和导出用到
+                'bankname',     // 列表和导出用到
+                'bank_open',    // 列表显示用到
+                'create_time'   // 列表和导出用到
+            ])
+            ->where('inizt', '=', RechargeOrder::INIZT_WITHDRAW)
+            ->where('status', '=', RechargeOrder::STATUS_SUCCESS)
+            ->orderBy('id', 'desc'); // 按照ID 倒序排序
         $grid->column('order_id', 'ID');
-
-
-        $grid->column('order_id', 'ID');
-        $grid->column('merchantid', '商户ID');
         $grid->column('orderid', '系统订单号'); // 直接对此字段查询
         $grid->column('amount', '金额');
-        $grid->column('notify_num', '回调次数');
         $grid->column('bank_open', '银行类型');
-        $grid->column('account', '收款账号')->hide();
+        $grid->column('account', '收款账号');
         $grid->column('bankname', '开户行');
-        $grid->column('yh_bq', '银行标签/E2E')->hide();
-        $grid->column('status', '订单状态')->display(function ($input) {
-            return isset(RechargeOrder::LIST_STATUS[$input]) ? RechargeOrder::LIST_STATUS[$input] : $input;
-        })->dot(RechargeOrder::getStatusDot());
-        $grid->column('notify_status', '订回掉状态')->display(function ($input) {
-            return isset(RechargeOrder::LIST_NOTIFY_STATUS[$input]) ? RechargeOrder::LIST_NOTIFY_STATUS[$input] : $input;
-        })->dot(RechargeOrder::getStatusDot());
-        $grid->column('inizt', '订单类型')->display(function ($input) {
-            return isset(RechargeOrder::LIST_INIZT[$input]) ? RechargeOrder::LIST_INIZT[$input] : $input;
-        })->dot(RechargeOrder::getStatusDot());
-
-
-        $grid->column('remarks', '备注');
-
-
         $grid->column('create_time', '添加时间')->display(function ($input) {
-            return formatTimeToString($input);
-        });
-
-        $grid->column('completetime', '完成时间')->display(function ($input) {
             return formatTimeToString($input);
         });
 
@@ -130,13 +112,6 @@ class RechargeOrder3Controller extends AdminController
         // 过滤器  查询字段
         $grid->filter(function (Grid\Filter $filter) {
             $filter->equal('order_id', 'ID')->width('350px');
-            $filter->equal('merchantid', '商户ID')->width('350px');
-            $filter->equal('orderid', '系统订单号')->width('350px');
-            $filter->equal('account', '收款账号')->width('350px');
-            $filter->equal('yh_bq', '银行标签/E2E')->width('350px');
-            $filter->equal('status', '状态')->select(RechargeOrder::LIST_STATUS);
-            $filter->equal('inizt', '订单类型')->select(RechargeOrder::LIST_INIZT);
-
             $filter->whereBetween('create_time', function ($q) {
                 $start = $this->input['start'] ?? strtotime('today');
                 $end = $this->input['end'] ?? strtotime('today');
